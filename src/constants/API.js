0b13fca81db8343195230cfc0bd1c399349c8cea
MyOldNick <denis.zin1999@gmail.com>
2021-04-08 16:25:01 +0300

    ADD: player, authors etcc

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,1 +1,2 @@
 export const API = 'http://localhost:5000/'
+export const STREAMS_API = 'http://127.0.0.1:8888/api/'
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,34 +1,69 @@
 //react
-import { useState, useEffect } from 'react'
-import { ReactFlvPlayer } from 'react-flv-player'
+import { useState, useEffect } from "react";
+import { ReactFlvPlayer } from "react-flv-player";
+//bootstrap
+import { Container, Row, Col } from "react-bootstrap";
+//constants
+import { API, STREAMS_API } from '../../constants/API'
 
-const Home = ()  => {
-    const [streams, setStreams] = useState({})
+const Home = () => {
+  const [streams, setStreams] = useState();
+  const [users, setUsers] = useState()
 
-    useEffect(() => {
+  useEffect(() => {
 
-        if(Object.keys(streams).length <= 0) {
+    //немного лапши и говнокода
+    if (!streams) {
+      fetch(`${STREAMS_API}streams`)
+        .then((res) => res.json())
+        .then(async (res) => { 
 
-            fetch(`http://127.0.0.1:8888/api/streams`)
-                .then(res => res.json())
-                .then(res => setStreams(res.live))
+            if(res && Object.keys(res).length !== 0) {
 
-        }
+                const keys = Object.keys(res.live)
 
-    }, [streams])
+                const obj = {keys}
 
-    return (
-        <div>
-            {Object.keys(streams).length > 0 ? Object.keys(streams).map(stream => (<div>
+                const json = await JSON.stringify(obj)
+    
+    
+                fetch(`${API}findUsers`, {
+                    method: "POST",
+                    body: json,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(data => data.json())
+                .then(data  => {
+                    setUsers(data)
+                    setStreams(res.live)
+                })
+            }
+        });
+    }
+
+  }, [streams]);
+
+
+
+  return (
+    <Container>
+      <Row className="mt-5">
+        {streams && Object.keys(streams).length > 0
+          ? Object.keys(streams).map((stream, index) => (
+              <Col md xl={4} key={index}>
                 <ReactFlvPlayer
-                    url={`http://127.0.0.1:8888/live/${stream}.flv`}
-                    heigh = "800px"
-                    width = "800px"
-                    isMuted={true}
+                  url={`http://127.0.0.1:8888/live/${stream}.flv`}
+                  isMuted={true}
                 />
-            </div>)) : 'Пусто'}
-        </div>
-    )
-}
+                <p>{users[index].login}</p>
+              </Col>
+            ))
+          : "Пусто"}
+      </Row>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
